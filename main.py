21dc2b01df2e18f84b1913b32cc2facaaf6aec3c
pxrzivaldev <dertammo.360@gmail.com>
2025-04-20 19:51:32 +0200

    added characters_count func in stats.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from stats import word_count
+from stats import *
 
 def main():
     booktext = get_book_text("books/frankenstein.txt")
     num_words = word_count(booktext)
+    charcount = characters_count(booktext)
     print(f"{num_words} words found in the document")
+    print(charcount)
     
 
 def get_book_text(path):
--- stats.py
+++ stats.py
@@ -2,3 +2,12 @@ def word_count(bookcontent):
     wordlist = bookcontent.split()
     wordcount = len(wordlist)
     return wordcount
+
+def characters_count(bookcontent):
+    uniqueChar = {}
+    for char in bookcontent.lower():
+        if(char in uniqueChar):
+            uniqueChar[char] += 1
+        else:
+            uniqueChar[char] = 1
+    return uniqueChar
